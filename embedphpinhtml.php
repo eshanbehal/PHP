9d9b57b8f808efa8932468833e796cfa9d9117d7
eshan <eshan786behal@gmail.com>
2019-12-03 14:25:59 +0530

    embeded php in html

---
+++ embedphpinhtml.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>embed php in html</title>
+  </head>
+  <body>
+    <h1>Embed php in html.</h1>
+    <p>
+    <?php
+echo "this is a parsagraph";
+     ?>
+   </p>
+     <?php echo "<p>this is another paragraph.</p>";
+      ?>
+  </body>
+</html>
