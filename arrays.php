1466946fb688f7cde27d296b794950e0806ae0e7
eshan <eshan786behal@gmail.com>
2019-12-10 14:05:30 +0530

    arrays.

---
+++ arrays.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+      Variables
+    </title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link href="https://unpkg.com/ionicons@4.4.4/dist/css/ionicons.min.css" rel="stylesheet">
+    <style>
+      h1{
+        color: purple;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <h1>Arrays: </h1>
+      <?php
+$array1 = array("eshan", "behal", "audi");
+echo "Array is: ";
+print_r($array1);
+$array2 = array("a"=>"eshan", "b"=>"behal", "c"=>"audi");
+print_r($array2);
+       ?>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js">
+    </script>
+    <script src="js/bootstrap.min.js">
+    </script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
